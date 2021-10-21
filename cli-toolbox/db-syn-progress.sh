9aaab356ed77bb5fd62e4215ac313ad87c6500a4
Sławomir Śledź <slawomir.sledz@gmail.com>
2021-10-22 00:18:49 +0200

    start using cardano-db-sync

---
+++ cli-toolbox/db-syn-progress.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source $dir/etc/config
+source $dir/lib/fun.sh
+source $dir/lib/lib.sh
+
+q_progress() {
+    cat <<EOF
+    select 100 * 
+      (
+        extract (epoch from (max (time) at time zone 'UTC')) - 
+        extract (epoch from (min (time) at time zone 'UTC'))
+      ) 
+        / 
+      (
+         extract (epoch from (now () at time zone 'UTC')) - 
+         extract (epoch from (min (time) at time zone 'UTC'))
+      ) as sync_percent from block
+EOF
+}
+
+q_progress | sql
+
+echo 'select now () - max (time) as behind_by from block' | sql
